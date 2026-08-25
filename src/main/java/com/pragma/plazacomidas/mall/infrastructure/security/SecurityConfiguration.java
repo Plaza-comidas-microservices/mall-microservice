8880d7f753681697501b8a8ff8ebaeb62d9428a4
Ing-Daron11 <daronmercadog@gmail.com>
2026-08-25 17:12:40 -0500

    fix: fix SonarQube findings

--- src/main/java/com/pragma/plazacomidas/mall/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/pragma/plazacomidas/mall/infrastructure/security/SecurityConfiguration.java
@@ -17,6 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private static final String OWNER = "OWNER";
+    private static final String EMPLOYEE = "EMPLOYEE";
+    private static final String CLIENT = "CLIENT";
+    private static final String ADMIN = "ADMIN";
+    private static final String ORDER_PATH = "/api/v1/order/**";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,14 +34,14 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(auth -> auth
                 .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/restaurant/*").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/v1/restaurant/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/api/v1/plate/**").hasRole("OWNER")
-                .antMatchers(HttpMethod.PATCH, "/api/v1/plate/**").hasRole("OWNER")
-                .antMatchers(HttpMethod.POST, "/api/v1/order/**").hasRole("CLIENT")
-                .antMatchers(HttpMethod.GET, "/api/v1/order/efficiency").hasRole("OWNER")
-                .antMatchers(HttpMethod.GET, "/api/v1/order/**").hasRole("EMPLOYEE")
-                .antMatchers(HttpMethod.PATCH, "/api/v1/order/*/cancel").hasRole("CLIENT")
-                .antMatchers(HttpMethod.PATCH, "/api/v1/order/**").hasRole("EMPLOYEE")
+                .antMatchers(HttpMethod.POST, "/api/v1/restaurant/**").hasRole(ADMIN)
+                .antMatchers(HttpMethod.POST, "/api/v1/plate/**").hasRole(OWNER)
+                .antMatchers(HttpMethod.PATCH, "/api/v1/plate/**").hasRole(OWNER)
+                .antMatchers(HttpMethod.POST, ORDER_PATH).hasRole(CLIENT)
+                .antMatchers(HttpMethod.GET, "/api/v1/order/efficiency").hasRole(OWNER)
+                .antMatchers(HttpMethod.GET, ORDER_PATH).hasRole(EMPLOYEE)
+                .antMatchers(HttpMethod.PATCH, "/api/v1/order/*/cancel").hasRole(CLIENT)
+                .antMatchers(HttpMethod.PATCH, ORDER_PATH).hasRole(EMPLOYEE)
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
